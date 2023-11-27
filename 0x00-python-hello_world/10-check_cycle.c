d80725171f22ffc8f39e954f5178fced404c0a21
nurelmallah <nourhan.elmallah@gmail.com>
2023-11-27 14:42:07 -0800

    test

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -1,26 +1,63 @@
+#include <stdio.h>
+#include <stdlib.h>
 #include "lists.h"
 
 /**
- * check_cycle - Checks if a singly linked list has a cycle.
- * @list: A pointer to the head of the linked list.
- *
- * Return: 0 if there is no cycle, 1 if there is a cycle.
+ * print_listint - prints all elements of a listint_t list
+ * @h: pointer to head of list
+ * Return: number of nodes
  */
-int check_cycle(listint_t *list)
+size_t print_listint(const listint_t *h)
 {
-	listint_t *slow = list, *fast = list;
+    const listint_t *current;
+    unsigned int n; /* number of nodes */
 
-	while (slow != NULL && fast != NULL && fast->next != NULL)
-	{
-		slow = slow->next;        /* Move one step at a time */
-		fast = fast->next->next;  /* Move two steps at a time */
+    current = h;
+    n = 0;
+    while (current != NULL)
+    {
+        printf("%i\n", current->n);
+        current = current->next;
+        n++;
+    }
 
-		if (slow == fast)
-		{
-			/* The pointers met, indicating a cycle */
-			return (1);
-		}
-	}
+    return (n);
+}
 
-	return (0);
+/**
+ * add_nodeint - adds a new node at the beginning of a listint_t list
+ * @head: pointer to a pointer of the start of the list
+ * @n: integer to be included in node
+ * Return: address of the new element or NULL if it fails
+ */
+listint_t *add_nodeint(listint_t **head, const int n)
+{
+    listint_t *new;
+
+    new = malloc(sizeof(listint_t));
+    if (new == NULL)
+        return (NULL);
+
+    new->n = n;
+    new->next = *head;
+    *head = new;
+
+    return (new);
+}
+
+/**
+ * free_listint - frees a listint_t list
+ * @head: pointer to list to be freed
+ * Return: void
+ */
+void free_listint(listint_t *head)
+{
+    listint_t *current;
+
+    while (head != NULL)
+    {
+        current = head;
+        head = head->next;
+        free(current);
+    }
 }
