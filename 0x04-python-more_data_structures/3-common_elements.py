20b0236444a2c66767917790fa904f4534c72aa1
nurelmallah <nourhan.elmallah@gmail.com>
2023-12-06 05:49:45 -0800

    adding advanced tasks + edit for 3

--- 0x04-python-more_data_structures/3-common_elements.py
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
-
-def common_elements(set_1, set_2):
-    """
-    A function that returns a set of
-    common elements in two sets
-    """
-    return(set_1 & set_2)
+def uniq_add(my_list=[]):
+    new = set(my_list)
+    result = 0
+    for i in new:
+        result += i
+    return result
